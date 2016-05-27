b3c73943e4dbf7cb14d6ea840c7720a3eb62c264
Javier Mayorga <javi_mayorga94@hotmail.com>
2016-05-26 19:02:16 -0600

    commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.use(function(req,res,next){
+  res.locals.project = "Mi App Start Page";
+  next();
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+module.exports = router;
